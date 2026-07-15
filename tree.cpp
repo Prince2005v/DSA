52ee3a68ba76e51b482197bab5702169deee724c
prince verma <8567prince@gmail.com>
2026-07-15 10:07:16 +0530

    your message

--- tree.cpp
+++ tree.cpp
@@ -26,11 +26,21 @@ Node* buildTree(vector<int> preorder){
 
 }
 
+void preOrder(Node* root){
+  if(root == NULL){
+    return;
+  }
+  cout << root -> data << " ";
+  preOrder(root -> left);
+  preOrder(root -> right);
+}
 int main() {
   vector<int> preorder = {1,2,-1,-1,3,4,-1,-1,5,-1,-1};
   Node* root = buildTree(preorder);
 
   cout << root -> data << endl;
+  preOrder(root);
+  cout  << endl;
 
 
   return 0;
