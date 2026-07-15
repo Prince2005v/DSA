d46e94560cf3d794846aa26a84a7e5becb3cf6db
prince verma <8567prince@gmail.com>
2026-07-15 10:31:22 +0530

    your message

--- tree.cpp
+++ tree.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 using namespace std;
 
 class Node {
@@ -51,16 +52,30 @@ void postOrder(Node* root){
   postOrder(root -> right);
   cout << root -> data << " ";
 }
+void  levelOrder(Node* root){
+  queue<Node*> q;
+  q.push(root);
+
+  while(!q.empty()){
+    Node* curr = q.front();
+    q.pop();
+
+    cout << curr -> data << " ";
+
+    if(curr -> left != NULL){
+      q.push(curr -> left);
+    }
+    if(curr -> right != NULL){
+      q.push(curr -> right);
+    }
+  }
+  cout << endl;
+} 
 int main() {
   vector<int> preorder = {1,2,-1,-1,3,4,-1,-1,5,-1,-1};
   Node* root = buildTree(preorder);
 
-  cout << root -> data << endl;
-  preOrder(root);
-  cout  << endl;
-  inOrder(root);
-  cout << endl;
-    postOrder(root);
+  levelOrder(root);
 
 
 
